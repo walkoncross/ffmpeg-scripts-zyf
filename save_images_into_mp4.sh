f520240bf9f490f43ac6d9f5482671cc92a75158
zhaoyafei <zhaoyafei0210@gmail.com>
2025-02-04 03:29:12 +0200

    update save_images_into_mp4.sh

--- save_images_into_mp4.sh
+++ save_images_into_mp4.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-input=$1
+input=$1 # format: images/frame_%04d.png
 output=output.mp4
 
 if [[ $# -gt 1 ]]; then
@@ -13,12 +13,14 @@ output=$3
 ffmpeg -r 25 \
     -i $input \
     -i $audio \
+    -vf "scale=iw-mod(iw\,2):ih-mod(ih\,2)" \
     -c:v libx264 \
     -pix_fmt yuv420p \
     $output
 else
 ffmpeg -r 25 \
     -i $input \
+    -vf "scale=iw-mod(iw\,2):ih-mod(ih\,2)" \
     -c:v libx264 \
     -pix_fmt yuv420p \
     $output
